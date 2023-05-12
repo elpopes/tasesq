5fc61c01e86747b63d480e306bb6048009ce41b7
lorenzo <lorenzotijerina@gmail.com>
2023-05-12 10:12:56 -0400

    Add Employment Law and Contact Us

---
+++ src/components/ContactUs.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ContactUs: React.FC = () => {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>
+        We would love to hear from you! Feel free to reach out to us using any of the contact methods below.
+      </p>
+      <h3>Contact Information</h3>
+      <ul>
+        <li>Address: 1920 Guadalupe, San Antonio, TX 78207</li>
+        <li>Phone: 210-231-0112</li>
+        <li>Email: tasesq@msn.com</li>
+      </ul>
+      {/* Insert Google Map component here */}
+      {/* Insert Email Form component (using SendGrid) here */}
+    </div>
+  );
+};
+
+export default ContactUs;
---
+++ src/components/EmploymentLaw.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const EmploymentLaw: React.FC = () => {
+  return (
+    <div>
+      <h2>Employment Law Services</h2>
+      <p>
+        At TASESQ Law Firm, we provide comprehensive employment law services to protect the rights and interests of employees and employers. With a specialization in federal cases, we offer strategic legal representation tailored to the complexities of employment law at the federal level.
+      </p>
+      <h3>Our Services</h3>
+      <ul>
+        <li>Discrimination and harassment claims</li>
+        <li>Wrongful termination and retaliation</li>
+        <li>Wage and hour disputes</li>
+        <li>Employment contract review and negotiation</li>
+        <li>Non-compete and confidentiality agreements</li>
+        <li>Employee benefits and ERISA matters</li>
+        <li>Whistleblower protection</li>
+        <li>Workplace investigations</li>
+        <li>Labor law and union disputes</li>
+      </ul>
+      <p>
+        Our experienced employment law attorneys understand the nuances of federal employment laws and regulations. We provide personalized guidance and representation to employees and employers, helping them navigate complex legal issues and strive for fair resolutions.
+      </p>
+      <p>
+        Whether you are facing workplace discrimination, need assistance with contract negotiations, or require representation in federal employment-related litigation, our team is dedicated to advocating for your rights and achieving favorable outcomes.
+      </p>
+      <p>
+        Contact us today to schedule a consultation and discuss your employment law matter with our experienced attorneys.
+      </p>
+    </div>
+  );
+};
+
+export default EmploymentLaw;
