7c104f61bbd03d40c298ac220270c0ec158126c8
lorenzo <lorenzotijerina@gmail.com>
2023-05-12 10:59:17 -0400

    use static iframe map without api

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 const Map: React.FC = () => {
   return (
     <div>
-      <h2>Location Map</h2>
-      <img
-        src={`https://maps.googleapis.com/maps/api/staticmap?center=1920+Guadalupe,+San+Antonio,+TX+78207&zoom=15&size=600x450&markers=1920+Guadalupe,+San+Antonio,+TX+78207`}
-        alt="Office Location"
-      />
-    </div>
+    <h2>Location Map</h2>
+    <iframe
+      title="Office Location"
+      width="600"
+      height="450"
+      loading="lazy"
+      allowFullScreen
+      src="https://www.google.com/maps?q=1920+Guadalupe,+San+Antonio,+TX+78207&output=embed"
+    ></iframe>
+  </div>
   );
 };
 
