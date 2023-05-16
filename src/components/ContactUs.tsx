31a4fb38d3f173f738959a22667b7740db05f256
lorenzo <lorenzotijerina@gmail.com>
2023-05-16 11:56:30 -0400

    style contact us

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -5,18 +5,22 @@ import EmailForm from './EmailForm';
 const ContactUs: React.FC = () => {
   return (
     <div className='container'>
-      <h2>Contact Us</h2>
-      <p>
-        We would love to hear from you! Feel free to reach out to us using any of the contact methods below.
-      </p>
-      <h3>Contact Information</h3>
-      <ul>
-        <li>Address: 1911 Guadalupe, San Antonio, TX 78207</li>
-        <li>Phone: 210-231-0112</li>
-        <li>Email: tasesq@msn.com</li>
-      </ul>
-      {<Map />}
-      {<EmailForm />}
+      <div className='contact-info'>
+        <h2>Contact Us</h2>
+        <p>
+          We would love to hear from you! Feel free to reach out to us using any of the contact methods below.
+        </p>
+        <h3>Contact Information</h3>
+        <ul>
+          <li>Address: 1911 Guadalupe, San Antonio, TX 78207</li>
+          <li>Phone: 210-231-0112</li>
+          <li>Email: tasesq@msn.com</li>
+        </ul>
+        <EmailForm />
+      </div>
+      <div className='map-wrapper'>
+        <Map />
+      </div>
     </div>
   );
 };
