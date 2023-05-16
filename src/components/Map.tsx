06266bd4d62b8877ff6c566462989ab2ae4ab4f2
lorenzo <lorenzotijerina@gmail.com>
2023-05-16 12:03:45 -0400

    style map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 
 const Map: React.FC = () => {
   return (
-    <div className='container'>
-    <h2>Location Map</h2>
-    <iframe
-      title="Office Location"
-      width="600"
-      height="450"
-      loading="lazy"
-      allowFullScreen
-      src="https://www.google.com/maps?q=1911+Guadalupe,+San+Antonio,+TX+78207&output=embed"
-    ></iframe>
-  </div>
+    <div style={{ width: '600px' }}>
+      <h2 style={{ marginBottom: '20px' }}>Location Map</h2>
+      <iframe
+        title="Office Location"
+        width="100%"
+        height="450"
+        loading="lazy"
+        allowFullScreen
+        src="https://www.google.com/maps?q=1911+Guadalupe,+San+Antonio,+TX+78207&output=embed"
+      ></iframe>
+    </div>
   );
 };
 
 export default Map;
+
