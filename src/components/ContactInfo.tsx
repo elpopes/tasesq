19fee1e0c791426c47a18f7b9eab135681ed420b
lorenzo <lorenzotijerina@gmail.com>
2023-05-17 17:19:45 -0400

    fix contact us component

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMapMarkerAlt, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faMapMarkerAlt, faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 interface ContactInfoProps {
     className?: string;
@@ -18,7 +18,13 @@ const ContactInfo: React.FC<ContactInfoProps> = ({ className }) => {
             <p>
                 <FontAwesomeIcon icon={faPhone} className="icon" /> 
                 <a href="tel:2102310112">
-                    210 231 0112
+                    (210) 231-0112
+                </a>
+            </p>
+            <p>
+                <FontAwesomeIcon icon={faEnvelope} className="icon" /> 
+                <a href="mailto:tasesq@gmail.com">
+                    email
                 </a>
             </p>
         </div>
--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import Map from './Map';
 import EmailForm from './EmailForm';
+import ContactInfo from './ContactInfo';
 
 const ContactUs: React.FC = () => {
   return (
-    <div className='contact-container'>
-      <div className='contact-info'>
-        <h2>Contact Us</h2>
-        <p>
-          We would love to hear from you! Feel free to reach out to us using any of the contact methods below.
-        </p>
-        <h3>Contact Information</h3>
-        <ul>
-          <li>Address: 1911 Guadalupe, San Antonio, TX 78207</li>
-          <li>Phone: 210-231-0112</li>
-          <li>Email: tasesq@msn.com</li>
-        </ul>
-        <EmailForm />
-      </div>
-      <div className='map-wrapper'>
-        <Map />
+    <div id="contact-us">
+      <div className='contact-container'>
+        <div className='contact-info'>
+          <h2>Contact Us</h2>
+          <p>
+            We would love to hear from you! Feel free to reach out to us using any of the contact methods below.
+          </p>
+          <h3>Contact Information</h3>
+          <ul>
+            <ContactInfo/>
+          </ul>
+          <EmailForm />
+        </div>
+        <div className='map-wrapper'>
+          <Map />
+        </div>
       </div>
     </div>
   );
