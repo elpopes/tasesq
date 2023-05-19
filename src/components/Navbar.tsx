32b9da1f1a85a60f83e7a0f1e19aa234e71f90a0
lorenzo <lorenzotijerina@gmail.com>
2023-05-19 11:30:21 -0400

    switch to Link from a ref

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
 import { ReactComponent as Logo } from './logo.svg';
 import ContactInfo from "./ContactInfo";
+import { Link } from 'react-router-dom';  // import Link here
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
   const node = useRef<HTMLUListElement>(null); 
 
   const handleClick = (e: any) => {
-
     if (node.current && !node.current.contains(e.target)) {
       setIsOpen(false);
     }
@@ -31,30 +31,17 @@ const Navbar: React.FC = () => {
     <ContactInfo className="contact-info-nav wide-screen" />
     <div onClick={() => setIsOpen(!isOpen)}>{isOpen ? 'X' : '☰'}</div>
     <ul ref={node} className={isOpen ? 'open' : ''}>
-      <li><a href="/">Home</a></li>
-          <li>
-            <a href="/immigration">Immigration</a>
-          </li>
-          <li>
-            <a href="/employment-law">Employment Law</a>
-          </li>
-          <li>
-            <a href="/accounting">Accounting</a>
-          </li>
-          <li>
-            <a href="/faqs">FAQs</a>
-          </li>
-          <li>
-            <a href="/about-us">About Us</a>
-          </li>
-          <li>
-            <a href="/contact-us">Contact Us</a>
-          </li>
-        </ul>
-    </nav>
-    </>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/immigration">Immigration</Link></li>
+      <li><Link to="/employment-law">Employment Law</Link></li>
+      <li><Link to="/accounting">Accounting</Link></li>
+      <li><Link to="/faqs">FAQs</Link></li>
+      <li><Link to="/about-us">About Us</Link></li>
+      <li><Link to="/contact-us">Contact Us</Link></li>
+    </ul>
+  </nav>
+  </>
   );
 }
 
 export default Navbar;
-
