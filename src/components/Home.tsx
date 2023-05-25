fadaaadb60799957efebc31a60b58e9f33c8bc3a
lorenzo <lorenzotijerina@gmail.com>
2023-05-25 11:03:22 -0400

    fill entire screen with banner

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -35,29 +35,32 @@ const Home: React.FC = () => {
 
 
     return (
-        <div className="home">
-          {slides.map((slide, index) => (
-            <HeroBanner 
-              title={slide.title} 
-              subtitle={slide.subtitle} 
-              image={slide.image} 
-              index={currentSlide === index ? 0 : index + 1}
-              key={index}
-            />
-          ))}
-      <h1>Welcome to TASESQ</h1>
-      <p>
-        We are a San Antonio-based federal law firm with over 40 years of
-        experience. Our seasoned team of legal professionals is dedicated to
-        providing the highest quality legal services to our clients. We
-        specialize in a broad range of federal legal matters and are committed
-        to delivering results for our clients.
-      </p>
-      <p>
-        Contact us today to learn more about how we can assist you with your
-        legal needs.
-      </p>
-    </div>
+<div className="home">
+  {slides.map((slide, index) => (
+    <HeroBanner 
+      title={slide.title} 
+      subtitle={slide.subtitle} 
+      image={slide.image} 
+      index={currentSlide === index ? 0 : index + 1}
+      key={index}
+    />
+  ))}
+  <div className="container">
+    <h1>Welcome to TASESQ</h1>
+    <p>
+      We are a San Antonio-based federal law firm with over 40 years of
+      experience. Our seasoned team of legal professionals is dedicated to
+      providing the highest quality legal services to our clients. We
+      specialize in a broad range of federal legal matters and are committed
+      to delivering results for our clients.
+    </p>
+    <p>
+      Contact us today to learn more about how we can assist you with your
+      legal needs.
+    </p>
+  </div>
+</div>
+
   );
 };
 
