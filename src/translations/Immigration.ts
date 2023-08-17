e683ad8da56e4af686fefa11335a3c0a9bef42bd
lorenzo <lorenzotijerina@gmail.com>
2023-08-17 13:23:41 -0400

    immigration copy

--- src/translations/Immigration.ts
+++ src/translations/Immigration.ts
@@ -3,7 +3,7 @@ export const immigrationText = {
       title: 'Immigration Services',
       introduction: 'At the Law Offices of Lorenzo Tijerina we provide comprehensive immigration services to individuals and businesses. Our team of experienced immigration attorneys is dedicated to helping clients navigate the complex immigration process with ease.',
       servicesTitle: 'Our Services',
-      servicesList: ['Visa applications', 'Green card applications', 'Citizenship applications', 'Deportation defense', 'Employment-based immigration', 'Family-based immigration', 'Immigration appeals', 'And more...'],
+      servicesList: ['Visa applications', 'Green card applications', 'Citizenship applications', 'Deportation proceedings', 'Employment-based immigration proceedings', 'Family-based immigration proceedings (I-130 petitions)', 'Immigration appeals before the Board of Immigration Appeals 2nd US Courts', 'And more...'],
       conclusion: 'Whether you are an individual seeking to reunite with your family or a business looking to hire international talent, we provide personalized solutions tailored to your unique immigration needs. Our team stays up to date with the latest immigration laws and regulations to ensure the best possible outcome for our clients.',
       callToAction: 'Contact us today to schedule a consultation and learn how we can assist you with your immigration matters.',
     },
