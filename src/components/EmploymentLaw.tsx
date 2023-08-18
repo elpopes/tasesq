8f6a8d0bccc297a4f0c8b0a050c5a9d40b623522
lorenzo <lorenzotijerina@gmail.com>
2023-08-18 16:09:55 -0400

    add additional seo for employment law

--- src/components/EmploymentLaw.tsx
+++ src/components/EmploymentLaw.tsx
@@ -1,31 +1,32 @@
 import React from 'react';
 import { employmentLawText } from '../translations/EmploymentLaw';
+import { LanguageContext, LanguageContextProps } from '../LanguageContext';
+import Helmet from 'react-helmet';
 
 const EmploymentLaw: React.FC = () => {
+  const { language } = React.useContext<LanguageContextProps>(LanguageContext); 
+  const t = employmentLawText[language];
+
   return (
     <div className='container'>
-      <h2>{employmentLawText.en.heading}</h2>
-      <p>
-        {employmentLawText.en.introduction}
-      </p>
-      <h3>{employmentLawText.en.servicesHeading}</h3>
+      <Helmet>
+        <title>{t.heading}</title>
+        <meta name="description" content={t.introduction} />
+        <meta name="keywords" content="employment law, discrimination, termination, workplace investigations, labor law, union disputes" />
+      </Helmet>
+      <h2>{t.heading}</h2>
+      <p>{t.introduction}</p>
+      <h3>{t.servicesHeading}</h3>
       <ul>
-        {employmentLawText.en.servicesList.map((service, index) => (
+        {t.servicesList.map((service, index) => (
           <li key={index}>{service}</li>
         ))}
       </ul>
-      <p>
-        {employmentLawText.en.paragraph1}
-      </p>
-      <p>
-        {employmentLawText.en.paragraph2}
-      </p>
-      <p>
-        {employmentLawText.en.contact}
-      </p>
+      <p>{t.paragraph1}</p>
+      <p>{t.paragraph2}</p>
+      <p>{t.contact}</p>
     </div>
   );
 };
 
 export default EmploymentLaw;
-
