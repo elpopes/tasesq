f8e67d7fafc8820c69fb6ae101d05364a425e568
lorenzo <lorenzotijerina@gmail.com>
2023-08-18 13:06:51 -0400

    Edit copy

--- src/translations/Home.ts
+++ src/translations/Home.ts
@@ -15,14 +15,14 @@ export const homeText = {
         },
       ],
       welcomeTitle: 'Welcome to the Law Offices of Lorenzo Tijerina, ESQ',
-      welcomeDescription1: 'We are a San Antonio/New York-based federal law firm with over 40 years of experience. Our legal professionals are dedicated to providing the highest quality legal services to our clients. We specialize in a broad range of federal legal matters and are committed to delivering results for our clients.',
+      welcomeDescription1: 'We are a San Antonio/New York-based federal law firm with over 30 years of experience. Our legal professionals are dedicated to providing the highest quality legal services to our clients. We specialize in a broad range of federal legal matters and are committed to delivering results for our clients.',
       welcomeDescription2: 'Contact us today to learn more about how we can assist you with your legal needs.',
     },
     es: {
       slides: [
         {
           title: "Bienvenidos a Nuestro Bufete de Abogados, LLC",
-          subtitle: "Proporcionando servicios legales de alta calidad durante más de 40 años",
+          subtitle: "Proporcionando servicios legales de alta calidad durante más de 30 años",
         },
         {
           title: "Equipo Legal con Experiencia",
@@ -34,7 +34,7 @@ export const homeText = {
         },
       ],
       welcomeTitle: 'Bienvenido a TASESQ',
-      welcomeDescription1: 'Somos un bufete de abogados federal con sede en San Antonio con más de 40 años de experiencia. Nuestro experimentado equipo de profesionales legales se dedica a proporcionar los servicios legales de la más alta calidad a nuestros clientes. Nos especializamos en una amplia gama de asuntos legales federales y estamos comprometidos a entregar resultados para nuestros clientes.',
+      welcomeDescription1: 'Somos un bufete de abogados federal con sede en San Antonio con más de 30 años de experiencia. Nuestro experimentado equipo de profesionales legales se dedica a proporcionar los servicios legales de la más alta calidad a nuestros clientes. Nos especializamos en una amplia gama de asuntos legales federales y estamos comprometidos a entregar resultados para nuestros clientes.',
       welcomeDescription2: 'Contáctenos hoy para obtener más información sobre cómo podemos ayudarlo con sus necesidades legales.',
     },
 };
