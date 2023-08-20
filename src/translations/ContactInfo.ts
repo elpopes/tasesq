ac19c5b6c8bace52c471f386ea26c2fd434cd54e
lorenzo <lorenzotijerina@gmail.com>
2023-08-20 17:04:49 -0400

    more changes

--- src/translations/ContactInfo.ts
+++ src/translations/ContactInfo.ts
@@ -7,7 +7,7 @@ export const contactInfoText = {
       addressLink: 'https://www.google.com/maps/search/?api=1&query=1911+Guadalupe,+San+Antonio,+TX+78207',
       nyAddressLink: 'https://goo.gl/maps/nubt44rVrzao5KNU7',
       phoneLink: 'tel:2102310112',
-      emailLink: 'mailto:tasesq@gmail.com'
+      emailLink: 'mailto:lorenzo@tijerinafedlaw.com'
     },
     es: {
       address: '1911 Guadalupe, San Antonio, TX 78207',
@@ -17,6 +17,6 @@ export const contactInfoText = {
       addressLink: 'https://www.google.com/maps/search/?api=1&query=1911+Guadalupe,+San+Antonio,+TX+78207',
       nyAddressLink: 'https://goo.gl/maps/nubt44rVrzao5KNU7',
       phoneLink: 'tel:8552468012',
-      emailLink: 'mailto:tasesq@gmail.com'
+      emailLink: 'mailto:lorenzo@tijerinafedlaw.com'
     },
   };
