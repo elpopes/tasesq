6b479693976450da7d3c05a7b559ff2e4484bffa
lorenzo <lorenzotijerina@gmail.com>
2023-08-29 17:34:22 -0400

    wrap useLocation

--- src/App.tsx
+++ src/App.tsx
@@ -11,32 +11,38 @@ import AboutUs from './components/AboutUs';
 import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
 
-const App: React.FC = () => {
+const AppRoutes: React.FC = () => {
   const location = useLocation();
-
-  useEffect(() => {
-    ReactGA.initialize('G-00C6NFXBXP');
-  }, []);
 
   useEffect(() => {
     ReactGA.pageview(location.pathname + location.search);
   }, [location]);
 
   return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/immigration" element={<Immigration />} />
+        <Route path="/employment-law" element={<EmploymentLaw />} />
+        <Route path="/accounting" element={<Accounting />} />
+        <Route path="/faqs" element={<FAQs />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+      </Routes>        
+      <Footer />
+    </div>
+  );
+};
+
+const App: React.FC = () => {
+  useEffect(() => {
+    ReactGA.initialize('G-00C6NFXBXP');
+  }, []);
+
+  return (
     <Router basename='/'>
-      <div className="App">
-        <Navbar />
-        <Routes>    
-          <Route path="/" element={<Home />} />
-          <Route path="/immigration" element={<Immigration />} />
-          <Route path="/employment-law" element={<EmploymentLaw />} />
-          <Route path="/accounting" element={<Accounting />} />
-          <Route path="/faqs" element={<FAQs />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-        </Routes>        
-        <Footer />
-      </div>
+      <AppRoutes />
     </Router>
   );
 };
